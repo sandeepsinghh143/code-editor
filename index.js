c707b8374d61bc651aee8b1d56dc0d4edeb9d28e
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2023-08-17 14:37:36 +0530

    some changes

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 $( document ).ready(function() {
     const result=$("iframe");
     result.find("head") && result.contents().find("head").append(`<style></style>`);
-    result.find("body") && result.contents().find("body").append(`<script></script>`);
     $(".toggle li").click(function(){
        $(`.${$(this).text().toLowerCase()}`).toggleClass("hide");
     });
 
     $(".run-button").click(function(){
     const html=$(".html textarea");
-    const css=$(".css textarea");
     const js=$(".js textarea");
     const result=$("iframe");
+    result.contents().find("body").remove("#myscript");
     result.contents().find("body").html(html.val());
-    result.contents().find("head").append(`<style>${css.val()}</style>`);
-    result.contents().find("body").append(`<script>${js.val()}</script>`);
+    result.contents().find("body").append(`<script id="myscript">${js.val()}</script>`);
     });
 
     $(".html textarea").keyup(function(e){
